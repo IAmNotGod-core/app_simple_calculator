62d7a885c3010da0ad6b4d8a2987b6b8224c0f1e
IAmNotGod <lintangkk09@gmail.com>
2022-12-25 01:34:48 +0700

    Add SharedPreferences

--- app/src/main/java/com/iamnotgod/calculator/MainActivity.java
+++ app/src/main/java/com/iamnotgod/calculator/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +13,10 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
     Button total;
     RecyclerView recList;
     ArrayList<Record> listRecord;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    Gson gson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,74 +43,74 @@ public class MainActivity extends AppCompatActivity {
         resultView = findViewById(R.id.txtResult);
         total = findViewById(R.id.btnTotal);
         recList = findViewById(R.id.recList);
-        initData();
 
-        recList.setAdapter(new RecordAdapter(listRecord));
-        recList.setLayoutManager(new LinearLayoutManager(this));
+        initData();
+        buildRecylerView();
 
         total.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int oprId = operator.getCheckedRadioButtonId();
-                operatorButton = findViewById(oprId);
-
-                if (oprId == R.id.oprAdd) {
-                    Add();
-                }
-                else if (oprId == R.id.oprSubtract) {
-                    Subtract();
-                }
-                else if (oprId == R.id.oprMultiply) {
-                    Multiply();
-                }
-                else if (oprId == R.id.oprDivide) {
-                    Divide();
-                }
+                Calculate();
             }
         });
     }
 
+    private void saveData() {
+        sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        gson = new Gson();
+        String json = gson.toJson(listRecord);
+        editor.putString("record list", json).apply();
+    }
+
     private void initData() {
-        this.listRecord = new ArrayList<>();
-        listRecord.add(new Record("10", "+", "5", "15"));
-        listRecord.add(new Record("20", "÷", "2", "10"));
-        listRecord.add(new Record("15", "-", "5", "10"));
-        listRecord.add(new Record("5", "×", "5", "25"));
+        sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        gson = new Gson();
+        String json = sharedPreferences.getString("record list", null);
+        Type type = new TypeToken<ArrayList<Record>>() {}.getType();
+        listRecord = gson.fromJson(json, type);
+
+        if (listRecord == null) {
+            listRecord = new ArrayList<>();
+        }
     }
 
-    public void Add() {
-        float n1 = Float.parseFloat(number1.getText().toString());
-        float n2 = Float.parseFloat(number2.getText().toString());
-
-        float result = n1 + n2;
-        resultView.setText(""+result);
-
+    private void buildRecylerView() {
+        recList.setHasFixedSize(true);
+        recList.setLayoutManager(new LinearLayoutManager(this));
+        recList.setAdapter(new RecordAdapter(listRecord));
     }
 
-    public void Subtract() {
+    private void Calculate() {
         float n1 = Float.parseFloat(number1.getText().toString());
         float n2 = Float.parseFloat(number2.getText().toString());
+        float result = 0;
+        String opr = "";
 
-        float result = n1 - n2;
+        int oprId = operator.getCheckedRadioButtonId();
+        operatorButton = findViewById(oprId);
+
+        if (oprId == R.id.oprAdd) {
+            result = n1 + n2;
+            opr = "+";
+            saveData();
+        }
+        else if (oprId == R.id.oprSubtract) {
+            result = n1 - n2;
+            opr = "-";
+            saveData();
+        }
+        else if (oprId == R.id.oprMultiply) {
+            result = n1 * n2;
+            opr = "×";
+            saveData();
+        }
+        else if (oprId == R.id.oprDivide) {
+            result = n1 / n2;
+            opr = "÷";
+            saveData();
+        }
         resultView.setText(""+result);
-
-    }
-
-    public void Multiply() {
-        float n1 = Float.parseFloat(number1.getText().toString());
-        float n2 = Float.parseFloat(number2.getText().toString());
-
-        float result = n1 * n2;
-        resultView.setText(""+result);
-
-    }
-
-    public void Divide() {
-        float n1 = Float.parseFloat(number1.getText().toString());
-        float n2 = Float.parseFloat(number2.getText().toString());
-
-        float result = n1 / n2;
-        resultView.setText(""+result);
-
+        listRecord.add(new Record(String.valueOf(n1), opr, String.valueOf(n2), String.valueOf(result)));
     }
 }
